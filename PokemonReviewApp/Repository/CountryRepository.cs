8ff24938fc302a55fae10b8e8b2e0f4f00600f18
Hasan Emre AVARİSLİ <emreavarisli@gmail.com>
2023-11-04 23:48:26 +0300

    category and country

---
+++ PokemonReviewApp/Repository/CountryRepository.cs
@@ -0,0 +1,33 @@
+﻿using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CountryRepository : ICountryRepository
+    {
+        public bool CountryExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Country GetCountry(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Country GetCountryByOwner(int ownerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Owner> GetOwnersFromCountry(int countryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
